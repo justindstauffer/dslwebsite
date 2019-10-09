92a5f565d64c059a9222d0e7d5803d2b77d169f7
Justin Stauffer <jstauffer@tenxweb.com>
2019-10-09 12:52:06 -0400

    10.9.2

--- src/components/imagebreak.js
+++ src/components/imagebreak.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import Background from "../images/lakehouse.jpg"
+import Background from "../images/landscape3.1.png"
 
 const ImageBreak = () => (
   <header
